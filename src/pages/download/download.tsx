7e9efdb1b0641e29024ab10dd8678cf6bb5b2de6
shenfeng1945 <823902826@qq.com>
2021-01-19 23:33:37 +0800

    新增下载页面

---
+++ src/pages/download/download.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { View, Text, Image, Navigator, Input } from "@tarojs/components";
+import IconFont from "../../iconfont";
+
+class DownloadPage extends Component {
+    render(){
+        return (
+            <View>下载</View>
+        )
+    }
+}
+
+export default DownloadPage;
