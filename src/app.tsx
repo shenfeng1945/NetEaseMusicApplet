a7291d2f7adc0b2d3d2b1ad8475a0b007eaa232a
shenfeng1945 <823902826@qq.com>
2021-01-20 17:30:25 +0800

    页面加载时初始化 av-weapp.js

--- src/app.tsx
+++ src/app.tsx
@@ -1,30 +1,36 @@
-import React, { Component } from 'react'
-import { Provider } from 'react-redux'
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import AV from "leancloud-storage/dist/av-weapp.js";
 
-import configStore from './store'
+import configStore from "./store";
 
-import './app.scss'
+import "./app.scss";
 
-const store = configStore()
+const store = configStore();
 
 class App extends Component {
-  componentDidMount () {}
+  componentDidMount() {
+    const APP_ID = "82InV9gcyTzIw099BrTuO1wd-gzGzoHsz";
+    const APP_KEY = "qCBb2prMElwslrBNaryawyER";
 
-  componentDidShow () {}
+    AV.init({
+      appId: APP_ID,
+      appKey: APP_KEY,
+      serverURLs: "https://netease.shenfeng1945.cn",
+    });
+  }
 
-  componentDidHide () {}
+  componentDidShow() {}
 
-  componentDidCatchError () {}
+  componentDidHide() {}
+
+  componentDidCatchError() {}
 
   // 在 App 类中的 render() 函数没有实际作用
   // 请勿修改此函数
-  render () {
-    return (
-      <Provider store={store}>
-        {this.props.children}
-      </Provider>
-    )
+  render() {
+    return <Provider store={store}>{this.props.children}</Provider>;
   }
 }
 
-export default App
+export default App;
